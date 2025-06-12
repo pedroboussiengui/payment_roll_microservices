780bfbc5eded8caac41f50ff5fb39063bd915315
pedroboussiengui <pedro.boussiengui.052@ufrn.edu.br>
2025-06-12 14:36:29 -0300

    feat: list job contracts in employee details

--- payment_roll/auth-app/src/routes/(binded)/employees/[id]/+page.svelte
+++ payment_roll/auth-app/src/routes/(binded)/employees/[id]/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-    import type { Employee } from "$lib/types";
+    import type { Contract, Employee } from "$lib/types";
 	import { onMount } from 'svelte';
     import payroll from "$lib/services/axios/payroll";
     import Navbar from "$lib/components/navbar.svelte";
 	import { page } from "$app/stores";
+	import { Icon, Plus } from "svelte-hero-icons";
 
     let employee: Employee | null = null;
+	let contracts: Contract[] = [];
     let error: string | null = null;
     
     $: employeeId = $page.params.id;
@@ -15,11 +17,19 @@
             console.log(employeeId);
             const res = await payroll.get(`/employees/${employeeId}`);
             employee = res.data;
+			const resContracts = await payroll.get(`/employees/${employeeId}/contracts`);
+			contracts = resContracts.data;
         } catch(err: any) {
             error = err.message;
             console.log(err);
         }
     });
+
+	$: hasContract = contracts.length > 0
+
+	async function admissionEvent(employeeId: string) {
+		console.log(`New admission event for employee ${employeeId}`);
+	}
 </script>
 
 <Navbar />
@@ -67,6 +77,24 @@
 				<p class="text-sm text-gray-500 font-medium">Father name</p>
 				<p class="text-base text-gray-800">{employee.fatherName}</p>
 			</div>
-		</div>
+		</div>/
+	</fieldset>
+
+	<fieldset class="border border-gray-300 rounded-lg p-6 m-2">
+		<legend class="text-lg font-semibold text-gray-700 px-2">Job contracts</legend>
+
+		<button 
+			on:click={() => admissionEvent(employeeId)}
+			class="flex items-center gap-x-2 bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">
+			<Icon src="{Plus}" solid size="24" /> Add job contract
+		</button>
+		
+		{#if !hasContract}
+			<p>No job contract was registred</p>
+		{:else}
+			{#each contracts as contract}
+				<p>{contract.matricula} - {contract.contractType} - {contract.position} - {contract.function} - {contract.entryDate}</p>
+			{/each}
+		{/if}
 	</fieldset>
 {/if}
